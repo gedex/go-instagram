fd2c4a62f706330ee641760225932be6f8542190
Akeda Bagus <admin@gedex.web.id>
2013-08-14 06:39:46 +0700

    Removed Location.CreatedTime as it doesn't exist.

--- instagram/locations.go
+++ instagram/locations.go
@@ -21,11 +21,10 @@ type LocationsService struct {
 
 // Location represents information about a location.
 type Location struct {
-	ID          string  `json:"id,omitempty"`
-	Name        string  `json:"name,omitempty"`
-	CreatedTime int64   `json:"created_time,string,omitempty"`
-	Latitude    float64 `json:"latitude,omitempty"`
-	Longitude   float64 `json:"longitude,omitempty"`
+	ID        string  `json:"id,omitempty"`
+	Name      string  `json:"name,omitempty"`
+	Latitude  float64 `json:"latitude,omitempty"`
+	Longitude float64 `json:"longitude,omitempty"`
 }
 
 // Get information about a location.
